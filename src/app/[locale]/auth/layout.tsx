35b69f13e06cef4a2c61bb52c5acf3db2df9df14
alaaadeln7 <alaaadeln7@gmail.com>
2025-06-29 22:34:47 +0300

    update auth layout

--- src/app/[locale]/auth/layout.tsx
+++ src/app/[locale]/auth/layout.tsx
@@ -1,20 +1,9 @@
 import React, { ReactNode } from "react";
-import Head from "next/head";
 import AuthHeader from "./AuthHeader";
 
-type LayoutProps = {
-  children: ReactNode;
-  title?: string;
-  description?: string;
-};
-
-const Layout = ({ children, description = "auth pages" }: LayoutProps) => {
+const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="min-h-screen flex flex-col">
-      <Head>
-        <meta name="description" content={description} />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <AuthHeader />
       <main>{children}</main>
     </div>
